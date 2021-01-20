b3f93e1b245d0947f1703113392929d52cca9be2
Qian Tang <q.tang6@student.unimelb.edu.au>
2021-01-20 23:03:03 +1100

    build a realtime chat app using vue.js

--- src/main.js
+++ src/main.js
@@ -2,9 +2,36 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import firebase from 'firebase'
 
 Vue.config.productionTip = false
 
+// const firebase = require("firebase");
+// Required for side-effects
+require("firebase/firestore");
+
+var firebaseConfig = {
+  apiKey: "AIzaSyCJVUfuld125dGmuRx_4U4QAdn_4yUK6CE",
+  authDomain: "vue-realtimechatapp.firebaseapp.com",
+  projectId: "vue-realtimechatapp",
+  storageBucket: "vue-realtimechatapp.appspot.com",
+  messagingSenderId: "542272461055",
+  appId: "1:542272461055:web:6b35e5aeb54785edef54ca"
+};
+
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+var db = firebase.firestore();
+
+//Make db accessible from everywhere
+window.db = db;
+
+//Disable deprecated features
+db.settings({
+  timestampsInSnapshots:true
+})
 new Vue({
   router,
   store,
